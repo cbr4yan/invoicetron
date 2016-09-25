7203dff0a0f22c14fa4e4a9456efd9f72b82f6bc
Brayan Castañeda <cbr4yan@gmail.com>
2016-09-25 21:36:47 +0200

    fix email

--- app/utils/index.js
+++ app/utils/index.js
@@ -5,11 +5,7 @@ exports.validateFormClient = (mode, data, callback) => {
   let error = null;
   if (!validator.isLength(data.name, { min: 3, max: 100 })) {
     error = { message: 'Nombre no valido.' };
-    return callback(error, data); 
-  }
-  if (!validator.isEmail(data.email)) {
-    error = { message: 'Email no valido.' };
-    return callback(error, data); 
+    return callback(error, data);
   }
   if (mode === 'add') {
     Clients.findOne({ dni: data.dni }, (err, client) => {
