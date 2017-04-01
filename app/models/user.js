4ae1e362157627b659c8f3e51c21760b4c044d8e
Brayan Castañeda <cbr4yan@gmail.com>
2017-04-01 11:36:32 +0200

    change bcrypt

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('../../config');
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = mongoose.Schema({
   name: {
